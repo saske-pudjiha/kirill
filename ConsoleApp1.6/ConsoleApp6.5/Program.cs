60c79377ca26bb2d8e06890c553e2be018bd0fc5
saske-pudjiha <kirigole2005@gmail.com>
2023-12-08 14:55:41 +0700

    6.5 6.6

--- ConsoleApp1.6/ConsoleApp6.5/Program.cs
+++ ConsoleApp1.6/ConsoleApp6.5/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
 
-namespace Program
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main()
+        Console.Write("Введите количество строк:");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите количество столбцов:");
+        int m = int.Parse(Console.ReadLine());
+
+        int[,] matrix = new int[n, m];
+
+        // Ввод матрицы
+        for (int i = 0; i < n; i++)
         {
+            Console.WriteLine($"Введите элементы {i + 1}-й строки, разделенные пробелом: ");
+            string[] input = Console.ReadLine().Split(' ');
             
+            for (int j = 0; j < m; j++)
+            {
+                matrix[i, j] = int.Parse(input[j]);
+            }
+        }
+        // Создание новой матрицы
+        int[,] newMatrix = new int[n, m + 1];
+        for (int i = 0; i < n; i++)
+        {
+            int count = 0;
+
+            for (int j = 0; j < m; j++)
+            {
+                newMatrix[i, j] = matrix[i, j];
+                count += matrix[i, j];
+            }
+
+            newMatrix[i, m] = count % 2;
+        }
+        // Вывод новой матрицы
+        Console.WriteLine("Новая матрица:");
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m + 1; j++)
+            {
+                Console.Write(newMatrix[i, j] + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
---
+++ ConsoleApp1.6/ConsoleApp6.6/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int length = random.Next(1, 10);
+        double[] array = GenerateRandomArray(length, random);
+        
+        List<double> positiveElements = new List<double>();
+        List<double> negativeElements = new List<double>();
+        
+        foreach (double number in array)
+        {
+            if (number > 0)
+            {
+                positiveElements.Add(number);
+            }
+            else if (number < 0)
+            {
+                negativeElements.Add(number);
+            }
+        }
+        
+        Console.WriteLine("Исходный массив:");
+        PrintArray(array);
+
+        Console.WriteLine("\nПоложительные элементы:");
+        PrintArray(positiveElements);
+
+        Console.WriteLine("\nОтрицательные элементы:");
+        PrintArray(negativeElements);
+
+        Console.ReadLine();
+    }
+
+    static double[] GenerateRandomArray(int length, Random random)
+    {
+        double[] array = new double[length];
+        for (int i = 0; i < length; i++)
+        {
+            array[i] = random.NextDouble() * (random.Next(0, 2) == 0 ? -1 : 1);
+        }
+        return array;
+    }
+
+    static void PrintArray(IEnumerable<double> array)
+    {
+        foreach (double number in array)
+        {
+            Console.Write(number + " ");
+        }
+    }
+}
